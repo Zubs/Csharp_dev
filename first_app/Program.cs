6b5cdc51ebbac52201324c3b8564b683a86f380e
Zubs <zubairidrisaweda@gmail.com>
2021-08-06 18:07:57 +0100

    Hello World

---
+++ first_app/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace first_app { // Namespace is the same as project name, for now
+    internal class Program {
+        public static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
