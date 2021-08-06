cc2d216e77ca76d6165f88fc39b94e1899dc9111
Zubs <zubairidrisaweda@gmail.com>
2021-08-06 20:12:57 +0100

    Variables

--- first_app/Program.cs
+++ first_app/Program.cs
@@ -32,6 +32,25 @@ namespace first_app { // Namespace is the same as project name, for now
             age = 21;
             
             Console.WriteLine(name + " is " + age + " years old");
+            
+            /*
+             * Data Types
+             */
+            string var1 = "This shit is crazy";
+           
+            /*
+             * Char can only use ''
+             * Char can only have one letter in them
+             */
+            char letter = 'B';
+            int var3 = 230;
+            int var4 = -56;
+            
+            // float and decimal are messed up -_-
+            float var5 = 12.535353535f;
+            double var6 = -0.00567;
+            decimal var7 = 12.3087383838m;
+            bool var8 = true;
         }
     }
 }
