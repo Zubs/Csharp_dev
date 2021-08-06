f40814f32e56cc2f772e2b852e4b05a1d2666ea3
Zubs <zubairidrisaweda@gmail.com>
2021-08-06 18:18:01 +0100

    Small triangle

--- first_app/Program.cs
+++ first_app/Program.cs
@@ -2,8 +2,16 @@
 
 namespace first_app { // Namespace is the same as project name, for now
     internal class Program {
-        public static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+        public static void Main() {
+            Console.WriteLine("Hello World!");  // Print out
+            
+            // Draw right angled triangle
+            Console.WriteLine("     /|");
+            Console.WriteLine("    / |");
+            Console.WriteLine("   /  |");
+            Console.WriteLine("  /   |");
+            Console.WriteLine(" /    |");
+            Console.WriteLine("/_____|");
         }
     }
 }
