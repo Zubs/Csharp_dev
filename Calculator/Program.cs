e85fe15f42f36f0a525be5407c68b6aa29958bbb
Zubs <zubairidrisaweda@gmail.com>
2021-08-09 00:43:21 +0100

    Real calculator

---
+++ Calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Calculator {
+    internal class Program {
+        public static void Main() {
+            Console.Write("Enter A Number: ");
+            double input1 = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Enter Another Number: ");
+            double input2 = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Choose an operator: ");
+            string op = Console.ReadLine();
+
+            if (op == "+") {
+                Console.WriteLine(input1 + input2);
+            } else if (op == "-") {
+                Console.WriteLine(input1 - input2);
+            } else if (op == "/") {
+                Console.WriteLine(input1 / input2);
+            } else if (op == "*") {
+                Console.WriteLine(input1 * input2);
+            }
+            else {
+                Console.Write("Invalid Input");
+            }
+        }
+    }
+}
