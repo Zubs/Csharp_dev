924afaf26e79962e228d04722b424b3eebf62af6
Zubs <zubairidrisaweda@gmail.com>
2021-08-08 23:23:22 +0100

    Mad Libs

---
+++ madLibs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace madLibs {
+    internal class Program {
+        public static void Main() {
+            string color, pluralNoun, celebrity;
+            
+            // Get color
+            Console.Write("Enter a color: ");
+            color = Console.ReadLine();
+            
+            // Get pluralNoun
+            Console.Write("Enter a plural noun: ");
+            pluralNoun = Console.ReadLine();
+            
+            // Get color
+            Console.Write("Enter a celebrity: ");
+            celebrity = Console.ReadLine();
+            
+            Console.WriteLine("Roses are " + color);
+            Console.WriteLine(pluralNoun + " are blue");
+            Console.WriteLine("I love " + celebrity);
+        }
+    }
+}
